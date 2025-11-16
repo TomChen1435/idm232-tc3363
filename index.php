3b2ea8c307e47931ead609163a253e014e94a1ae
Tom Chen <tc3363@drexel.edu>
2025-11-16 13:41:24 -0500

    Set up homepage

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wooden Ladle Recipes</title>
+    <link rel="stylesheet" href="/css/general.css">
+</head>
+<body>
+
+    <?php include './php/header.php' ?>
+
+    <main class="menu"><?php include './php/home.php' ?></main>
+    <main class="recipe"><?php include './php/recipe.php' ?></main>
+    <main class="filter"><?php include './php/filter_results.php' ?></main>
+    <main class="no-result"><?php include './php/no_result.php' ?></main>
+    
+    <footer>
+        <p>&copy; 2025 Wooden Ladle Recipes, for educational use only.</p>
+    </footer>
+
+    <style>
+        footer {
+            align-items: center;
+            background-color: var(--bg-black);
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: flex-end;
+            padding: var(--gap16) var(--gap24);
+
+            p {
+                color: var(--txt-white);
+                font-size: 1rem;
+            }
+        }
+    </style>
+
+    <script src="./postload.js"></script>
+
+</body>
+</html>
