cfdf8825fd097d729a03e61501ebfdfb0e1461b1
Tom Chen <tc3363@drexel.edu>
2025-11-26 18:31:29 -0500

    Finish card designs

--- index.php
+++ index.php
@@ -5,10 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wooden Ladle Recipes</title>
     <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/card_grid.css">
+    <link rel="stylesheet" href="css/cards.css">
 </head>
 <body>
 
     <?php include("components/header.php"); ?>
+
+    <main>
+        <template>
+            <a href="" class="card">
+                <picture>
+                    <source srcset="" type="image/avif">
+                    <img src="" alt="">
+                </picture>
+                <section>
+                    <h1></h1>
+                    <h2></h2>
+                </section>
+            </a>
+        </template>
+    </main>
 
     <?php include("components/footer.php"); ?>
     
